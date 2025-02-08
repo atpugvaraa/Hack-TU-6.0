f0e4aaf7ccd7178908639e9b9e08937fd8d1cb77
TheStupidOneHere <bawasingh70440@gmail.com>
2025-02-08 15:46:01 +0530

    Added tag

--- Backend/django/myapp/myapp/ models.py
+++ Backend/django/myapp/myapp/ models.py
@@ -2,14 +2,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Listing(models.Model):
   title = models.CharField(max_length=250)
   author = models.ForeignKey(User,
                              on_delete=models.CASCADE,
-                             related_name='listing')
+                             related_name='listings')
   imglink = models.TextField()
-  publishdate = models.DateTimeField(timezone.now)
+  publish = models.DateTimeField(default=timezone.now)
   location = models.CharField(max_length=250)
   price = models.IntegerField()
   quantitiy = models.IntegerField()
+  tags = models.ManyToManyField(Tag, related_name='listings', blank=True)
+
+  def __str__(self):
+      return self.title
   
